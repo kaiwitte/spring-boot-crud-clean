0d0bb114d7a469c718ee94d15ec9527edc88ef32
kaiwitte <who2contact@witte-consulting.com>
2026-04-21 23:06:30 +0200

    checking spotless on commit

--- src/test/java/com/witteconsulting/cruddemo/RoomIT.java
+++ src/test/java/com/witteconsulting/cruddemo/RoomIT.java
@@ -1,17 +1,16 @@
 package com.witteconsulting.cruddemo;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import com.witteconsulting.cruddemo.model.RoomRequestDto;
 import com.witteconsulting.cruddemo.model.RoomResponseDto;
+import java.util.UUID;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-
-import java.util.UUID;
-
-import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class RoomIT {
@@ -22,7 +21,7 @@ class RoomIT {
     @Test
     void shouldCreateRoom() {
         final RoomRequestDto roomRequest = new RoomRequestDto();
-        roomRequest.setName("Conference Room A");
+        roomRequest.setName("Conference Room B");
 
         final ResponseEntity<RoomResponseDto> response =
                 restTemplate.postForEntity("/rooms", roomRequest, RoomResponseDto.class);
@@ -30,7 +29,7 @@ class RoomIT {
         assertThat(response.getStatusCode()).isEqualTo(HttpStatus.CREATED);
         assertThat(response.getBody()).isNotNull();
         assertThat(response.getBody().getId()).isNotNull();
-        assertThat(response.getBody().getName()).isEqualTo("Conference Room A");
+        assertThat(response.getBody().getName()).isEqualTo("Conference Room B");
     }
 
     @Test
