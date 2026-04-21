6b3486f2bb3fa3f09550f0faedce7b89eef74505
kaiwitte <who2contact@witte-consulting.com>
2026-04-21 20:28:40 +0200

    mapper used in service class

--- src/main/java/com/witteconsulting/cruddemo/entity/RoomEntity.java
+++ src/main/java/com/witteconsulting/cruddemo/entity/RoomEntity.java
@@ -5,7 +5,6 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -15,7 +14,6 @@ import lombok.Setter;
 import java.util.UUID;
 
 @Entity
-@Table(name = "rooms")
 @Getter
 @Setter
 @Builder
--- src/main/java/com/witteconsulting/cruddemo/service/RoomService.java
+++ src/main/java/com/witteconsulting/cruddemo/service/RoomService.java
@@ -2,6 +2,7 @@ package com.witteconsulting.cruddemo.service;
 
 import com.witteconsulting.cruddemo.api.RoomsApiDelegate;
 import com.witteconsulting.cruddemo.entity.RoomEntity;
+import com.witteconsulting.cruddemo.mapper.RoomMapper;
 import com.witteconsulting.cruddemo.model.RoomRequestDto;
 import com.witteconsulting.cruddemo.model.RoomResponseDto;
 import com.witteconsulting.cruddemo.repository.RoomRepository;
@@ -20,13 +21,11 @@ public class RoomService implements RoomsApiDelegate {
 
     @Override
     public ResponseEntity<RoomResponseDto> newRoom(final RoomRequestDto roomRequestDto) {
-        final RoomEntity entity =
-                RoomEntity.builder().name(roomRequestDto.getName()).build();
+        final RoomEntity entity = RoomMapper.INSTANCE.dtoToEntity(roomRequestDto);
 
         final RoomEntity savedEntity = roomRepository.save(entity);
 
-        final RoomResponseDto response =
-                new RoomResponseDto().id(savedEntity.getId()).name(savedEntity.getName());
+        final RoomResponseDto response = RoomMapper.INSTANCE.entityToDto(savedEntity);
 
         return ResponseEntity.created(URI.create("/rooms/" + savedEntity.getId()))
                 .body(response);
@@ -36,11 +35,8 @@ public class RoomService implements RoomsApiDelegate {
     public ResponseEntity<RoomResponseDto> getRoom(final UUID roomId) {
         return roomRepository
                 .findById(roomId)
-                .map(entity -> {
-                    final RoomResponseDto response =
-                            new RoomResponseDto().id(entity.getId()).name(entity.getName());
-                    return ResponseEntity.ok(response);
-                })
+                .map(RoomMapper.INSTANCE::entityToDto)
+                .map(ResponseEntity::ok)
                 .orElse(ResponseEntity.notFound().build());
     }
 }
